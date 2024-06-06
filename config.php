1cae8fcf2c9a62e96d14a806d9c1498796e3b4c3
Atif Ehsan <atife605@gmail.com>
2024-06-06 13:26:53 +0200

    improvement

--- config.php
+++ config.php
@@ -4,4 +4,4 @@ define("HOST_NAME", "localhost");
 define("USER_NAME", "root");
 define("PASSWORD", "");
 define("DATABASE_NAME", "kaufland");
-define("XML_FILE", "feds.xml");
+define("XML_FILE", "feeds.xml");
